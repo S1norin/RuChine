86f0f44ff6f765bd00cd6496e285ea0d23c38445
sinorin <mishacat_2015@mail.com>
2023-02-11 09:08:58 +0800

    main

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from nltk.stem import WordNetLemmatizer
 
-user_input = 'Боль'
+user_input = input()
 soup = BeautifulSoup(requests.get(f"https://ruwordnet.ru/ru/search/{user_input}").text, 'html.parser')
 soup.head.clear()
-counter = 0
 
-print(soup.find("div", class_="sense").next_sibling == "\n")
-for i in soup.find("div", class_="sense"):
-    #    print((soup.find("div", class_="sense").find_all("div", class_="sense")))
-    parsed = BeautifulSoup(str(i), 'html.parser')
-    result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
-    for words in result:
-        if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)):
-            print(words.get_text(strip=True).strip(","))
+printed = []
 
-for i in soup.find("div", class_="sense").next_siblings:
-    #    print((soup.find("div", class_="sense").find_all("div", class_="sense")))
-    parsed = BeautifulSoup(str(i), 'html.parser')
-    result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
-    for words in result:
-        if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)):
-            print(words.get_text(strip=True).strip(","))
+if None == soup.find("div", class_="sense"):
+    print(f"Слова {user_input.lower()} нет в RuWordNet. Введите другое.")
+else:
+    for i in soup.find("div", class_="sense"):
+        parsed = BeautifulSoup(str(i), 'html.parser')
+        result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
+        for words in result:
+            if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)) \
+                    and not (words.get_text(strip=True).strip(",") in printed):
+                print(words.get_text(strip=True).strip(","))
+                printed.append(words.get_text(strip=True).strip(","))
+
+    for i in soup.find("div", class_="sense").next_siblings:
+        #    print((soup.find("div", class_="sense").find_all("div", class_="sense")))
+        parsed = BeautifulSoup(str(i), 'html.parser')
+        result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
+        for words in result:
+            if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)):
+                print(words.get_text(strip=True).strip(","))
