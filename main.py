fa061e8723041e15c26edc6792f2cd656596e42f
Sinorin <mishacat_2015@mail.ru>
2023-03-02 18:09:33 +0700

    main

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for sentence in user_input:
             pass
             # print(f"Слова {token.lemma_.lower()} нет в RuWordNet. Введите другое.")
         else:
-            print("------------------------------------------")
+#            print("------------------------------------------")
             for i in soup.find("div", class_="sense"):
                 lst_of_words_and_synonyms = []
                 parsed = BeautifulSoup(str(i), 'html.parser')
@@ -32,12 +32,13 @@ for sentence in user_input:
                     output.append([token.lemma_, *lst_of_words_and_synonyms])
 print(output)
 
-#
-'''for i in soup.find("div", class_="sense").next_siblings:
-            #    print((soup.find("div", class_="sense").find_all("div", class_="sense")))
-            parsed = BeautifulSoup(str(i), 'html.parser')
-            result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
-            for words in result:
-                if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)):
-                    print(words.get_text(strip=True).strip(","))
+
+'''
+for i in soup.find("div", class_="sense").next_siblings:
+    print((soup.find("div", class_="sense").find_all("div", class_="sense")))
+    parsed = BeautifulSoup(str(i), 'html.parser')
+    result = BeautifulSoup(str(parsed), 'html.parser').find_all("div", class_="sense")
+    for words in result:
+        if not ("." in words.get_text(strip=True) and "i" in words.get_text(strip=True)):
+            print(words.get_text(strip=True).strip(","))
 '''
